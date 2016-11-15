e4bf8cc8520e2317dbd9a7315f78452cef051ca4
sumansouravsingh <sumansouravsingh999@gmail.com>
2016-11-15 18:42:02 -0500

    Bar Graph and minor bug fixes

--- graph.js
+++ graph.js
@@ -1,9 +1,9 @@
-var arr = {text1:100,text2:300,text3:200,text4:350,text5:400,text6:180,text7:200,text8:100, text9:250, text10:250 };
-histoGram(arr,400,400);
-lineGraph(arr,200,400);
-pieChart(arr,400,400);
-
-function histoGram(arr,w,h)
+//var arr = {text1:100,text2:300,text3:200,text4:350,text5:400,text6:180,text7:200,text8:600, text9:250, text10:250, text11:300, text12: 400, text13:370, text14:220 };
+//histoGram(arr,400,1200,6,0);
+//lineGraph(arr,200,400,6,1);
+//pieChart(arr,400,400);
+//barGraph(arr,400,400,2);
+function histoGram(arr,w,h,nLines,isVerticalLines)
 {
    if(w<400)
       w=400;
@@ -31,7 +31,8 @@ function histoGram(arr,w,h)
    ctx.font = "7px Arial";
    ctx.strokeStyle="#cccccc";
    ctx.lineWidth=1;      
-   drawGraphLines(ctx,w,h,arr,length,max);
+   drawXGraphLines(ctx,w,h,arr,length,max,nLines);
+   drawYGraphLines(ctx,w,h,arr,length,max,isVerticalLines);
    drawHistoGram(ctx,w,h,arr,length,max);
    ctx.beginPath();
    ctx.strokeStyle="#000000";
@@ -50,13 +51,66 @@ function histoGram(arr,w,h)
    ctx.save();
    ctx.translate(0,h);
    ctx.rotate(Math.PI/180*-90);
-   ctx.fillText("Y-Axis Text",gWidth/2,0.1*w);
+   ctx.fillText("Y-Axis Text",gHeight/2,0.1*w);
    ctx.restore();
-   ctx.fillText("X-Axis Text",gWidth/2,h-10);
+   ctx.fillText("X-Axis Text",gWidth/2,gHeight+30);
+}
+
+function barGraph(arr,w,h,nLines,isHorizontalLines)
+{
+   if(w<400)
+      w=400;
+   if(h<400)
+      h=400;
+   var gWidth = 0.8*w;
+   var gHeight = 0.9*h;
+   var gwStart = 0.2*w;
+   var ghStart = 0.1*h;
+   var max=0,sum=0;
+   var length= Object.keys(arr).length;
+   for(var i=0;i<length;i++)
+   {
+      if(arr[Object.keys(arr)[i]]>max)
+      max = arr[Object.keys(arr)[i]];
+      sum+=arr[Object.keys(arr)[i]]; 
+   }
+   var cElem = document.createElement("canvas");
+   cElem.setAttribute("id","barGraph");
+   cElem.setAttribute("height",h);
+   cElem.setAttribute("width",w);
+   document.getElementById('mycanvas').appendChild(cElem);
+   var c = cElem;
+   var ctx = c.getContext("2d");
+   ctx.font = "7px Arial";
+   ctx.strokeStyle="#cccccc";
+   ctx.lineWidth=1;      
+   drawXBarGraphLines(ctx,w,h,arr,length,max,isHorizontalLines);
+   drawYBarGraphLines(ctx,w,h,arr,length,max,nLines);
+   drawBarGraph(ctx,w,h,arr,length,max,nLines);
+   ctx.beginPath();
+   ctx.strokeStyle="#000000";
+   ctx.lineWidth=2;      
+   ctx.moveTo(gwStart,0);
+   ctx.lineTo(gwStart,gHeight);
+   ctx.stroke();
+   ctx.closePath();
+   ctx.beginPath();
+   ctx.moveTo(gwStart,gHeight);
+   ctx.lineTo(w,gHeight);
+   ctx.stroke();
+   ctx.closePath();
+   ctx.font = "12px Arial";
+   ctx.fillStyle = "#000000";  
+   ctx.save();
+   ctx.translate(0,h);
+   ctx.rotate(Math.PI/180*-90);
+   ctx.fillText("Y-Axis Text",gHeight/2,0.1*w-10);
+   ctx.restore();
+   ctx.fillText("X-Axis Text",gWidth/2,0.9*h+30);
 }
 
 
-function lineGraph(arr,w,h)
+function lineGraph(arr,w,h,nLines,isVerticalLines)
 {
    if(w<400)
       w=400;
@@ -85,7 +139,8 @@ function lineGraph(arr,w,h)
    ctx.font = "7px Arial";
    ctx.strokeStyle="#cccccc";
    ctx.lineWidth=1;      
-   drawGraphLines(ctx,w,h,arr,length,max);
+   drawXGraphLines(ctx,w,h,arr,length,max,nLines);
+   drawYGraphLines(ctx,w,h,arr,length,max,isVerticalLines);
    ctx.lineWidth=2;   
    drawLineGraph(ctx,w,h,arr,length,max);
    //Drawing the X and Y lines.
@@ -106,7 +161,7 @@ function lineGraph(arr,w,h)
    ctx.save();
    ctx.translate(0,h);           //To draw the text in vertical direction, first translate the canvas by its height
    ctx.rotate(Math.PI/180*-90);  //Now rotate the canvas by -90 deg.
-   ctx.fillText("Y-Axis Text",gWidth/2,0.1*w);  //Draw the Y-axis text in vertical direction. 
+   ctx.fillText("Y-Axis Text",gHeight/2,0.1*w);  //Draw the Y-axis text in vertical direction. 
    ctx.restore();                               //Restore the canvas to its saved state.
    ctx.fillText("X-Axis Text",gWidth/2,h-10); //Draw the X-axis text.
 }
@@ -136,53 +191,6 @@ function pieChart(arr,w,h)
 }
 
 
-
-/*
-//Bar Graph         
-//Pie Chart
-var cElem = document.createElement("canvas");
-cElem.setAttribute("id","mepi");
-cElem.setAttribute("height",h);
-cElem.setAttribute("width",w);
-document.getElementById('mycanvas').appendChild(cElem);
-var d = cElem;
-var cntxt = d.getContext("2d");
-drawPiCircle(cntxt);
-
-//Line Graph
-var cElem2 = document.createElement("canvas");
-cElem2.setAttribute("id","meli");
-cElem2.setAttribute("height",h);
-cElem2.setAttribute("width",w);
-document.getElementById('mycanvas').appendChild(cElem2);
-var e = cElem2;
-var c1 = e.getContext("2d");
-c1.beginPath();
-c1.strokeStyle="#000000";
-c1.lineWidth=2;      
-c1.moveTo(36,0);
-c1.lineTo(36,360);
-c1.stroke();
-c1.closePath();
-c1.rotate(Math.PI/2);
-
-c1.rotate(-Math.PI/2);
-c1.fillText("Heading",180,390);
-c1.beginPath();
-c1.strokeStyle="#000000";
-c1.lineWidth=2;      
-c1.moveTo(36,360);
-c1.lineTo(400,360);
-c1.stroke();
-c1.closePath();
-c1.font = "7px Arial";
-c1.strokeStyle="#cccccc";
-drawGraphLines(c1);
-c1.lineWidth=1;  
-c1.lineWidth=2;   
-//console.log(arr[Object.keys(arr)[0]]);   
-drawLineGraph(c1);
-*/
 //Draw Line Graph
 function drawLineGraph(ctx,w,h,arr,length,max)
 {
@@ -194,7 +202,7 @@ function drawLineGraph(ctx,w,h,arr,length,max)
    var gWidth = 0.8*w;
    var gHeight = 0.9*h;
    var a1=0.8*w;var a2=0.1*w;
-   var x=gWidth/length-0.01*w;
+   var x=gWidth/(length+1);
    ctx.fillText(arr[Object.keys(arr)[0]],x+gwStart,gHeight-(arr[Object.keys(arr)[0]]*0.8*h/max)-5);
    for(var i=1;i<length;i++)
    {
@@ -206,7 +214,81 @@ function drawLineGraph(ctx,w,h,arr,length,max)
       ctx.closePath();
       j++;
    }
+}
 
+//Draw the lines parallel to Y-axis. (X constant and Y changing)
+function drawYBarGraphLines(ctx,w,h,arr,length,max,nLines)
+{
+   var j=0;
+   var ghStart = 0.1*h;
+   var gwStart = 0.2*w;
+   var gWidth = 0.8*w;
+   var gHeight = 0.9*h;
+   var a1=0.8*w;var a2=0.1*w;
+   var x=gHeight/length-0.004*h;
+   for(var i=w-a2;i>=gwStart;i=i-(0.7/nLines)*w)
+   {
+      ctx.beginPath();
+      ctx.moveTo(i,0);
+      ctx.lineTo(i,gHeight);
+      ctx.stroke();
+      if(max%nLines!=0)
+         ctx.fillText(parseFloat(max/nLines*(nLines-j)).toFixed(1),i-7.5,gHeight+10);
+      else
+         ctx.fillText(max/nLines*(nLines-j),i-7.5,gHeight+10);
+      ctx.closePath();
+      j++;
+   } 
+}
+
+//Draw the lines parallel to X-axis. (Y constant and X changing)
+function drawXBarGraphLines(ctx,w,h,arr,length,max,isHorizontalLines)
+{
+   var j=0;
+   var ghStart = 0.1*h;
+   var gwStart = 0.2*w;
+   var gWidth = 0.8*w;
+   var gHeight = 0.9*h;
+   var a1=0.8*w;var a2=0.1*w;
+   var x=gHeight/(length+1);
+   
+   if(isHorizontalLines)
+   {
+      for(var i=0;i<length;i++)
+      {
+         ctx.beginPath();
+         ctx.moveTo(gwStart,x*(i+1));
+         ctx.lineTo(w,x*(i+1));
+         ctx.stroke();
+         ctx.closePath();
+         j++;
+      }
+   }
+   j=0;
+   for(var i=0;i<length;i++)
+   {
+      ctx.fillText(Object.keys(arr)[j],a2,x*(i+1));
+      j++;
+   }      
+}
+//Draw the bars. (Rectangles, start points: x-> gwStart, y->x*(i+0.75), lengths: x->value of arr[i], y->0.5*x
+function drawBarGraph(ctx,w,h,arr,length,max,nLines)
+{
+   var l = length;
+   var ghStart = 0.1*h;
+   var gwStart = 0.2*w;
+   var gWidth = 0.8*w;
+   var gHeight = 0.9*h;
+   var x=gHeight/(length+1);
+
+   for(var i=0;i<l;i++)
+   {
+      ctx.beginPath();
+      ctx.fillStyle = getRandomColor();
+      ctx.fillRect(gwStart,x*(i+0.75),arr[Object.keys(arr)[i]]*0.7*w/max,0.5*x);//(0.7 = number of vertical lines(8)*difference between each line(0.0875))
+      ctx.fillText(arr[Object.keys(arr)[i]],arr[Object.keys(arr)[i]]*0.7*w/max+gwStart+5,x*(i+1));
+      ctx.closePath();
+   }
 }
 
 //Draw Pie Chart
@@ -226,10 +308,11 @@ function drawPiCircle(ctx,w,h,arr,length,sum)
       ctx.fill();
       ctx.closePath();
    }
+
 }
 
 //Drawing the graph lines
-function drawGraphLines(ctx,w,h,arr,length,max)
+function drawYGraphLines(ctx,w,h,arr,length,max,isVerticalLines)
 {
    var j=0;
    var ghStart = 0.1*h;
@@ -237,31 +320,49 @@ function drawGraphLines(ctx,w,h,arr,length,max)
    var gWidth = 0.8*w;
    var gHeight = 0.9*h;
    var a1=0.8*w;var a2=0.1*w;
-   var x=gWidth/length-0.01*w;
-   for(var i=0;i<length;i++)
-   {
-      ctx.beginPath();
-      ctx.moveTo(gwStart+x*(i+1),ghStart);
-      ctx.lineTo(gwStart+x*(1+i),gHeight);
-      ctx.stroke();
-      ctx.fillText(Object.keys(arr)[j],gwStart+x*(i+1)-7.5,gHeight+10);
-      ctx.closePath();
-      j++;
+   var x=gWidth/(length+1);
+   if(isVerticalLines){
+      for(var i=0;i<length;i++)
+      {
+         ctx.beginPath();
+         ctx.moveTo(gwStart+x*(i+1),ghStart);
+         ctx.lineTo(gwStart+x*(i+1),gHeight);
+         ctx.stroke();
+         ctx.closePath();
+         j++;
+      }   
    }
    j=0;
-   for(var i=gHeight;i>=ghStart;i=i-ghStart)
+   for(var i=0;i<length;i++)
+   {
+      ctx.fillText(Object.keys(arr)[j],gwStart+x*(i+1)-7.5,gHeight+10);
+      j++;
+   }
+   
+}
+function drawXGraphLines(ctx,w,h,arr,length,max,nLines)
+{
+   var j=0;
+   var ghStart = 0.1*h;
+   var gwStart = 0.2*w;
+   var gWidth = 0.8*w;
+   var gHeight = 0.9*h;
+   var a1=0.8*w;var a2=0.1*w;
+   var x=gWidth/length-0.008*w;
+   for(var i=gHeight;i>=ghStart;i=i-(8/nLines)*ghStart)
    {
       ctx.beginPath();
       ctx.moveTo(gwStart,i);
       ctx.lineTo(w,i);
       ctx.stroke();
-      if(max%8!=0)
-         ctx.fillText(parseFloat(max/8*(j)).toFixed(1),0.15*w,i+2.5);
+      if(max%nLines!=0)
+         ctx.fillText(parseFloat(max/nLines*(j)).toFixed(1),0.15*w,i+2.5);
       else
-         ctx.fillText(max/8*(j),0.15*w,i+2.5);
+         ctx.fillText(max/nLines*(j),0.15*w,i+2.5);
       ctx.closePath();
       j++;
    } 
+
 }
 
 //Draw Histo Gram(rectangle)
@@ -275,7 +376,7 @@ function drawHistoGram(ctx,w,h,arr,length,max)
    var gHeight = 0.9*h;
    var a1=0.8*w;
    var a2=0.1*w;
-   var x=gWidth/length-0.01*w;
+   var x=gWidth/(length+1);
    for(var i=0;i<l;i++)
    {
       ctx.beginPath();
